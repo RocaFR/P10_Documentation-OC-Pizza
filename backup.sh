12c988308a3450674e7a42bb5cd6932deba48ca8
Bryan Ferreras-Roca <59340663+RocaFR@users.noreply.github.com>
2022-03-17 19:00:30 +0100

    Hide files

--- backup.sh
+++ backup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 sudo systemctl stop postgresql
-sudo rsync -aR --delete --files-from=/home/oc/items_to_backup.txt / /home/oc/backup/
+sudo rsync -aR --delete --files-from=/home/oc/.files_to_backup.txt / /home/oc/backup/
 sudo systemctl start postgresql
--- configure_backup.sh
+++ configure_backup.sh
@@ -2,6 +2,7 @@
 
 echo "Téléchargement du script de backup..."
 wget -L https://raw.githubusercontent.com/RocaFR/P10_Documentation-OC-Pizza/main/backup.sh?token=GHSAT0AAAAAABQXT7UZFUDIGQ4A6KUYPKF2YRTOI6Q -O .backup.sh
+wget -L https://raw.githubusercontent.com/RocaFR/P10_Documentation-OC-Pizza/main/.files_to_backup.txt?token=GHSAT0AAAAAABQXT7UZZDCTY2WNNDUDX2VGYRTO4PQ -O .files_to_backup.txt
 chmod +x .backup.sh
 
 echo "Configuration du cron..."
